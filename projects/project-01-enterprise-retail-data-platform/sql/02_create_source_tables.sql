6309ae9100d1f7660b3c5803dda9de2a08d91777
sheshadrihymavathi-1001 <sheshadrihymavathi@gmail.com>
2026-09-09 22:43:36 +0530

    Add source schema SQL script

---
+++ projects/project-01-enterprise-retail-data-platform/sql/02_create_source_tables.sql
@@ -0,0 +1,5 @@
+-- Project 01 source schema script.
+-- Tables: customers, products, stores, orders, order_items, payments,
+-- inventory, order_status_history.
+
+-- TODO: add the validated local source-schema DDL.
